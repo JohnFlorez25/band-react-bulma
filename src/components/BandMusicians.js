55920f1bef8fa4c72f1cec0dc6e0acd3d607a541
John Faber <john.florez25@gmail.com>
2020-05-19 13:03:55 -0500

    Practicando estado

--- src/components/BandMusicians.js
+++ src/components/BandMusicians.js
@@ -6,13 +6,26 @@ class BandMusicians extends React.Component {
     constructor(props){
       super(props);
       this.state = {
-
+         number : 20
       }
   }
+
+  _aumentarContador = () => {
+      this.setState({
+          number : this.state.number + 1
+      })
+  }
+
+  _disminuirContador = () => {
+    this.setState({
+        number : this.state.number - 1
+    })
+}
 
   render() {
     //const musicTest = [1, 2, 3];
     return (
+      <>
       <div className='columns is-centered' style={{ padding: '2rem' }}>
         {this.props.musicians.map(  ( musician ) => {
           return <BandMusician 
@@ -21,6 +34,26 @@ class BandMusicians extends React.Component {
                 />;
         })}
       </div>
+      <div className="box is-centered">
+            <h1 className="title">
+                {this.state.number}
+            </h1>
+            <div className="buttons is-centered">
+                <button 
+                    onClick={this._disminuirContador}
+                    className="button is-info"
+                >
+                    Decrementar
+                </button>
+                <button 
+                    onClick={this._aumentarContador}
+                    className="button is-success"
+                >
+                    Incrementar
+                </button>
+            </div>
+      </div>
+      </>
     );
   }
 }
